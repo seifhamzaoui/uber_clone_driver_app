fd5b1e46e3a48edbeda94e05658c6f4af336b091
seifhamzaoui <seifhamz232@gmail.com>
2022-07-26 13:10:26 +0200

    init

---
+++ android/app/src/main/kotlin/com/example/uber_clone_driver_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uber_clone_driver_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/routes/router.dart
@@ -0,0 +1,7 @@
+import 'package:auto_route/auto_route.dart';
+
+@MaterialAutoRouter(
+  replaceInRouteName: 'Page,Route',
+  routes: <AutoRoute>[],
+)
+class $AppRouter {}
